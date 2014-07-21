27936824830a1a828e8a15a3e16e75abdd9f7b71
Travis Metz <travisrmetz@gmail.com>
2014-07-20 22:35:12 -0400

    TRM changes to cachematrix 7-20-14

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This group of functions takes a matrix and returns its inverse.  If that inverse has already
+## been calculated then it does not recompute such inverse, but, rather, returns the previously
+## calculated inverse
 
-## Write a short comment describing this function
 
+## Sets up the functions used in cacheSolve for determining if already calculated
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        get<-function()x
+        setinverse<-function(inverse) m<<-inverse
+        getinverse<-function() m
+        list(set=set,get=get,
+             setinverse=setinverse,
+             getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of x; returns cached data if has already
+## been previously called with same input
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m<-x$getinverse()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        data<-x$get()
+        m<-solve(data,...)
+        x$setinverse(m)
+        m
 }
